55b04c0262bd79920a102ef57eadce6722ed111b
R1047 <13479390798@163.com>
2023-10-10 10:56:25 +0800

    added

--- scripts/train_PCLMs.sh
+++ scripts/train_PCLMs.sh
@@ -1,5 +1,5 @@
 model_name_or_path=gpt2-medium
 device_num=3
-output_dir=./ckpt
+output_dir=../ckpt
 
 python ../train_PCLMs.py --model_name_or_path $model_name_or_path --output_dir $output_dir --device_num $device_num
